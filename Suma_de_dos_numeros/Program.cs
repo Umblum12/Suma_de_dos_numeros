52e44d82f2f6e8d2ccb6aa61fd7b8a6c19f1e941
Umblum12 <68798247+Umblum12@users.noreply.github.com>
2022-03-25 03:38:44 -0700

    Agregar archivos de proyecto.

---
+++ Suma_de_dos_numeros/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Suma_de_dos_numeros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declarado de variables
+            int num1 = 10;
+            int num2 = 20;
+            int resultado;
+            //Colocar el procedimiento
+            resultado = num1 + num2;
+            //Imprimir en pantalla el resultado
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine("El numero uno es: " + num1);
+            Console.WriteLine("El numero dos es: " + num2);
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine("La suma de los dos numeros es: " + resultado);
+            Console.WriteLine("--------------------------------------------");
+            Console.ReadKey(true);
+        }
+    }
+}
